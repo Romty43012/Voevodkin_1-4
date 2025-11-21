1ace0d76e0f82a0fea52459e7b5e83e2ada7e924
Рома <157241379+Romty43012@users.noreply.github.com>
2025-11-21 22:24:24 +0700

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-    return (
-        <footer className="bg-dark text-white p-2 mt-3">
-            Воеводкин Роман
-        </footer>
-    );
+  return (
+    <footer className="bg-dark text-white p-2 fixed-bottom">
+      Воеводкин Роман
+    </footer>
+  );
 }
